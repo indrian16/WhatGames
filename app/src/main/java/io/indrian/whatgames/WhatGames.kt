4293f342e521ae63aeacabbc5fcfd0e69d9acfc8
INDRIAN <indrian16@outlook.co.id>
2021-07-24 17:35:24 +0800

    inject module from app

--- app/src/main/java/io/indrian/whatgames/WhatGames.kt
+++ app/src/main/java/io/indrian/whatgames/WhatGames.kt
@@ -1,5 +1,25 @@
 package io.indrian.whatgames
 
 import android.app.Application
+import io.indrian.core.di.databaseModule
+import io.indrian.core.di.networkModule
+import io.indrian.core.di.repositoryModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
 
-class WhatGames : Application()
+class WhatGames : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger(level = Level.ERROR)
+            androidContext(this@WhatGames)
+            modules(
+                databaseModule,
+                networkModule,
+                repositoryModule
+            )
+        }
+    }
+}
