b5094c1a0324aa5608af80d252a3e2fabf39b40f
INDRIAN <indrian16@outlook.co.id>
2021-08-01 23:48:03 +0800

    implement network ssl pinner

--- core/src/main/java/io/indrian/core/di/NetworkModule.kt
+++ core/src/main/java/io/indrian/core/di/NetworkModule.kt
@@ -2,6 +2,7 @@ package io.indrian.core.di
 
 import com.haroldadmin.cnradapter.NetworkResponseAdapterFactory
 import io.indrian.core.data.source.remote.network.ApiService
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
@@ -11,6 +12,14 @@ import java.util.concurrent.TimeUnit
 
 val networkModule = module {
     single {
+        val hostname = "api.rawg.io"
+        val certificatePinner = CertificatePinner.Builder()
+            .add(hostname, "sha256/+WSYXXW0rd5TnILDGuJvshU5aExcOMlLxvQBPOT4PS0=")
+            .add(hostname, "sha256/hS5jJ4P+iQUErBkvoWBQOd1T7VOAYlOVegvv1iMzpxA=")
+            .add(hostname, "sha256/RI9CUmPUOpUk2vdVMSZDWj+wtoQO5k9MSCSM9w4grmU=")
+            .add(hostname, "sha256/FEzVOUp4dF3gI0ZVPRJhFbSJVXR+uQmMH65xhs1glH4=")
+            .build()
+
         OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .addInterceptor { chain ->
@@ -24,6 +33,7 @@ val networkModule = module {
             }
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
     single {
