3f641695e33dcd33875f66489c6e57f98199d775
INDRIAN <indrian16@outlook.co.id>
2021-08-01 23:34:24 +0800

    implement sqlite security

--- core/src/main/java/io/indrian/core/di/DatabaseModule.kt
+++ core/src/main/java/io/indrian/core/di/DatabaseModule.kt
@@ -3,16 +3,22 @@ package io.indrian.core.di
 import androidx.room.Room
 import io.indrian.core.data.source.local.room.AppDatabase
 import io.indrian.core.utils.CoreUtils
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val databaseModule = module {
     single {
+
+        val passPhrase = SQLiteDatabase.getBytes("indrian.io".toCharArray())
+        val factory = SupportFactory(passPhrase)
+
         Room.databaseBuilder(
             androidContext(),
             AppDatabase::class.java,
             CoreUtils.DB_NAME
-        ).fallbackToDestructiveMigration().build()
+        ).fallbackToDestructiveMigration().openHelperFactory(factory).build()
     }
 
     single {
